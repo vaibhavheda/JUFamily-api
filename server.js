f4eefb1f965569b6aebd32815488f797ae3d9221
Vai-devil <Vaibhavheda79@gmail.com>
2019-06-21 19:44:30 +0530

    Databse URL aND ssl added

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ const database = {
     ]
 }
 
+app.all('*', (req, res, next) => {
+    var origin = req.get('origin');
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 //root
 app.get('/', (req, res) => {
     res.send("this is working");
