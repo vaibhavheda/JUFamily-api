8ad35055f5ec823993f0439553d946a8e677c6ee
Vai-devil <Vaibhavheda79@gmail.com>
2019-06-21 21:06:28 +0530

    Databse URL aND ssl added

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ let app = express();
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
-    res.header("Access-Control-Allow-Headers", "GET,POST,OPTIONS,PUT,PATCH,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,PATCH,DELETE");
     res.header('Access-Control-Allow-Credentials', true);
     next();
 });
