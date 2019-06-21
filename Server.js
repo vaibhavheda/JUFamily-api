596cb5f56d5123ef3e60442b7e251d30951f3701
Vai-devil <Vaibhavheda79@gmail.com>
2019-06-21 17:27:58 +0530

    AddingServer

---
+++ Server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+// const pg = require('pg');
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        user: 'Vaibhav',
+        password: 'root',
+        database: 'family'
+    }
+});
+
+
+
+
+//this is to create server
+let app = express();
+
+
+//middleware
+
+app.use(cors());
+app.use(bodyParser.json({}));
+
+
+
+const database = {
+    users: [
+        {
+            id: 1,
+            name: "vaibhav",
+            email: "vaibhav.heda799@gmail.com",
+            linkedin: "",
+            image: ""
+        },
+        {
+            id: 2,
+            name: "vaib",
+            email: "vaibhav799@gmail.com",
+            linkedin: "",
+            image: ""
+        },
+        {
+            id: 3,
+            name: "v",
+            email: "vaibhav.heda@gmail.com",
+            linkedin: "",
+            image: ""
+        }
+    ]
+}
+
+//root
+app.get('/', (req, res) => {
+    res.send("this is working");
+});
+
+app.get('/add-family', (req, res) => {
+    res.send("okey");
+    console.log(database.users[1]);
+})
+
+//add-family
+
+app.post('/add-family', (req, res) => {
+    const { name, email, jobtitle, linkedin, image } = req.body;
+    console.log(name, email, jobtitle);
+    db('member').returning('*').insert({
+        name: name,
+        email: email,
+        jobtitle: jobtitle,
+        linkedin: linkedin,
+        image: image
+    }).then(res.json("Successful"));
+
+});
+
+app.get('/family', (req, res) => {
+    db.select('*').from('member')
+        .then(member => {
+            res.json(member);
+        })
+})
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log("app is running");
+})
+
+/*
+/ --> this is working resp
+/family --> GET
+/add-family --> POST
+*/
